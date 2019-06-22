e4066cfcf2a2b197f618ae1deb87e1b862c30ce5
Maruf <36636180+Madraasasa@users.noreply.github.com>
2019-06-23 00:23:48 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotos
 
 
 def convert_time_to_string(dt):
-    return f"{dt.hour+5}:{dt.minute:02}"
-
+    print(dt.hour % 24)
+    if dt.hour % 24 >= 10:
+        return f"{dt.hour+5}:{dt.minute:02}"
+    return f"0{dt.hour+5}:{dt.minute:02}"
 
 def time_has_changed(prev_time):
     return convert_time_to_string(datetime.datetime.now()) != prev_time
